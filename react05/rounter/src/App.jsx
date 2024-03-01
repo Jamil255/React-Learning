04c02633ab2c27cde282959fb4e5edd374341be1
unknown <jamilafzal255@gmail.com>
2024-03-01 11:59:21 +0500

    navLink

--- react05/rounter/src/App.jsx
+++ react05/rounter/src/App.jsx
@@ -3,10 +3,12 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
 import Contact from './pages/Contact'
+import Navbar from './Components/Navbar'
 
 function App() {
   return (
-    <>
+      <>
+          <Navbar/>
       <Routes>
         <Route index element={<Home/>} />
         <Route path="/About" element={<About/>} />
