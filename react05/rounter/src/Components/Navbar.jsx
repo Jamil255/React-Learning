3a474a104dc5d57dad2f876a1dd4a101d1f991af
unknown <jamilafzal255@gmail.com>
2024-03-02 17:35:51 +0500

    tailwind

--- react05/rounter/src/Components/Navbar.jsx
+++ react05/rounter/src/Components/Navbar.jsx
@@ -3,43 +3,7 @@ import { Link, NavLink } from 'react-router-dom'
 import styles from './app.module.css'
 
 function Navbar() {
-  return (
-    <>
-      <ul className="d-flex gap-5 w-100 bg-dark mt-4 p-4 list ps-5">
-        <li gap-5>
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive ? styles.activeLink : styles.normalLink
-            }
-          >
-            Home
-          </NavLink>
-        </li>
-        <li></li>
-        <li>
-          <NavLink
-            to="/contact"
-            className={({ isActive }) =>
-              isActive ? styles.activeLink : styles.normalLink
-            }
-          >
-            Contact
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-            to="/page"
-            className={({ isActive }) =>
-              isActive ? styles.activeLink : styles.normalLink
-            }
-          >
-            Page
-          </NavLink>
-        </li>
-      </ul>
-    </>
-  )
+  return <></>
 }
 
 export default Navbar
